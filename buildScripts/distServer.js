f04b5ac099e7f75f0517394f7197fac757c531e7
Ricardo de Carvalho Correia <rdcc89@gmail.com>
2017-05-18 23:19:53 -0300

    Finishing Pluralsight course

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -9,15 +9,6 @@ const app = express();
 app.use(compression());
 app.use(express.static("dist"));
 
-app.get("/users", function(req, res) {
-	// Hard coding for simplicity. Pretend this hits a real database
-	res.json([
-		{"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
-		{"id": 2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
-		{"id": 3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmail.com"}
-	]);
-});
-
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
